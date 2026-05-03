8d2d293ca5d0d105b57ce5c0663a4f617bc15099
Divya.B <kavya0308181@gmail.com>
2026-05-03 10:30:22 +0530

    Deploy contracts to Sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,10 +16,11 @@ export default defineConfig({
       chainType: "l1",
       url: "http://127.0.0.1:8545",
     },
-    render: {
+    sepolia: {
       type: "http",
       chainType: "l1",
-      url: "https://realchain-node.onrender.com",
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts: ["c0b13d3563755f05e81604c74df5dd631f379063bb084d185ffba7af03c93ca8"],
     },
   },
 });
